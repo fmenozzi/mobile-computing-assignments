bc616945a8305e8f584e9ed37502a7ccb062b5cd
Federico Menozzi <federicogmenozzi@gmail.com>
2017-01-27 13:06:49 -0500

    Create entire grid programmatically

--- hw1/app/src/main/java/com/example/menozzi/hw1/MainActivity.java
+++ hw1/app/src/main/java/com/example/menozzi/hw1/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.menozzi.hw1;
 
+import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int GRID_SIZE = 4;
 
     enum TargetState {
         BLACK,
         WHITE,
     }
 
+    TargetState targetState = TargetState.BLACK;
+
     Button mBlackButton;
     Button mWhiteButton;
 
-    TargetState targetState = TargetState.BLACK;
-
     int primaryColor;
     int secondaryColor;
+
+    TableLayout mGridTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,39 @@ public class MainActivity extends AppCompatActivity {
 
         primaryColor   = ContextCompat.getColor(this, R.color.colorPrimaryDark);
         secondaryColor = ContextCompat.getColor(this, R.color.colorSecondary);
+
+        mGridTable = (TableLayout) findViewById(R.id.grid_table);
+        for (int r = 0; r < GRID_SIZE; r++) {
+            TableRow row = new TableRow(this);
+            row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                                                          TableRow.LayoutParams.WRAP_CONTENT));
+            for (int c = 0; c < GRID_SIZE; c++) {
+                boolean rowEven = r % 2 == 0;
+                boolean colEven = c % 2 == 0;
+
+                boolean darkSquare = rowEven == colEven;
+
+                int padding = dipToPixels(this, 4);
+
+                int w = dipToPixels(this, 64);
+                int h = dipToPixels(this, 64);
+
+                TextView cell = new TextView(this);
+                cell.setLayoutParams(new TableRow.LayoutParams(c+1));
+                cell.setBackgroundColor(darkSquare ? primaryColor : secondaryColor);
+                cell.setTextColor(darkSquare ? secondaryColor : primaryColor);
+                cell.setGravity(Gravity.CENTER);
+                cell.setText(String.valueOf(r*GRID_SIZE + c));
+                cell.setTextSize(40);
+                cell.setPadding(padding, padding, padding, padding);
+                cell.setWidth(w);
+                cell.setHeight(h);
+
+                row.addView(cell);
+            }
+
+            mGridTable.addView(row);
+        }
     }
 
     public void onBlackClick(View view) {
@@ -51,5 +95,10 @@ public class MainActivity extends AppCompatActivity {
         mBlackButton.setTextColor(primaryColor);
 
         targetState = TargetState.WHITE;
+    }
+
+    public int dipToPixels(Context context, int dip) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, metrics);
     }
 }
