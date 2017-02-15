76844adbae8fbae349ff0882aab6befe1ac3e37f
Federico Menozzi <federicogmenozzi@gmail.com>
2017-02-15 14:29:42 -0500

    Draw dummy grid lines

--- hw2/app/src/main/java/com/example/menozzi/hw2/PlotActivity.java
+++ hw2/app/src/main/java/com/example/menozzi/hw2/PlotActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 
 public class PlotActivity extends AppCompatActivity {
 
-    Axis mXAxis;
-    Axis mYAxis;
+    PlotView mPlotView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plot);
+
+        mPlotView = (PlotView) findViewById(R.id.plotview);
+        mPlotView.setXAxis(new Axis(0.0, 5.0, 1.0, 0.0, 5.0, 0.0, "X Axis"));
+        mPlotView.setYAxis(new Axis(0.0, 5.0, 1.0, 0.0, 5.0, 0.0, "Y Axis"));
+        mPlotView.invalidate();
     }
 }
--- hw2/app/src/main/java/com/example/menozzi/hw2/PlotView.java
+++ hw2/app/src/main/java/com/example/menozzi/hw2/PlotView.java
@@ -2,25 +2,51 @@ package com.example.menozzi.hw2;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class PlotView extends View {
 
+    Axis mXAxis;
+    Axis mYAxis;
+
+    Paint mGridPaint = new Paint();
+
     public PlotView(Context context) {
         super(context);
     }
-
     public PlotView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-
     public PlotView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public void setXAxis(Axis xAxis) {
+        mXAxis = xAxis;
+    }
+    public void setYAxis(Axis yAxis) {
+        mYAxis = yAxis;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        int w = canvas.getWidth();
+        int h = canvas.getHeight();
+
+        mGridPaint.setColor(Color.GRAY);
+        mGridPaint.setAntiAlias(true);
+
+        for (int x = 0; x < w; x += w/8) {
+            canvas.drawLine(x, 0.0f, x, h, mGridPaint);
+        }
+
+        for (int y = 0; y < h; y += h/8) {
+            canvas.drawLine(0.0f, y, w, y, mGridPaint);
+        }
     }
 }
