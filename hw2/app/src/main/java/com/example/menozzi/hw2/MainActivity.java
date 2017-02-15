4a89fb1493c6c28fe5e5f8ea3e1e5be4b0a4b56e
Federico Menozzi <federicogmenozzi@gmail.com>
2017-02-15 13:36:30 -0500

    Setup temp button to call custom plot view

--- hw2/app/src/main/java/com/example/menozzi/hw2/MainActivity.java
+++ hw2/app/src/main/java/com/example/menozzi/hw2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.menozzi.hw2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void tempClickCallback(View v) {
+        Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show();
     }
 }
