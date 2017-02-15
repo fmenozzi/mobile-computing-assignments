beae4626df3070c644817333d65d48cac56e2e75
Federico Menozzi <federicogmenozzi@gmail.com>
2017-02-15 13:39:08 -0500

    Add skeleton for custom plot view

---
+++ hw2/app/src/main/java/com/example/menozzi/hw2/PlotView.java
@@ -0,0 +1,26 @@
+package com.example.menozzi.hw2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PlotView extends View {
+
+    public PlotView(Context context) {
+        super(context);
+    }
+
+    public PlotView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PlotView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
