e62779912a9449cd2246a291a8bcdfa608dadb69
Federico Menozzi <federicogmenozzi@gmail.com>
2017-02-16 16:02:28 -0500

    Streaming buffer seems to work!

--- hw2/app/src/main/java/com/example/menozzi/hw2/activities/MainActivity.java
+++ hw2/app/src/main/java/com/example/menozzi/hw2/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.menozzi.hw2.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     void tempClickCallback(View v) {
         Intent intent = new Intent(this, PlotActivity.class);
+        intent.setData(Uri.parse((v.getId() == R.id.sensor1_button) ? "Sensor 1" : "Sensor 2"));
         startActivity(intent);
     }
 }
--- hw2/app/src/main/java/com/example/menozzi/hw2/activities/PlotActivity.java
+++ hw2/app/src/main/java/com/example/menozzi/hw2/activities/PlotActivity.java
@@ -1,15 +1,21 @@
 package com.example.menozzi.hw2.activities;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.menozzi.hw2.Axis;
+import com.example.menozzi.hw2.FixedCircularBuffer;
 import com.example.menozzi.hw2.views.PlotView;
 import com.example.menozzi.hw2.R;
 
 public class PlotActivity extends AppCompatActivity {
 
     PlotView mPlotView;
+    TextView mTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,24 @@ public class PlotActivity extends AppCompatActivity {
         mPlotView.setXAxis(new Axis(0.0, 5.0, 1.0, "X Axis"));
         mPlotView.setYAxis(new Axis(0.0, 5.0, 1.0, "Y Axis"));
         mPlotView.invalidate();
+
+        mTextView = (TextView) findViewById(R.id.textview);
+        mTextView.setText(getIntent().getData().toString());
+
+        new CountDownTimer(10000, 500) {
+            private FixedCircularBuffer<Integer> buffer = new FixedCircularBuffer<>(4);
+            int i = 0;
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                buffer.add(i++);
+                mTextView.setText(buffer.toString());
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(PlotActivity.this, "Done!", Toast.LENGTH_SHORT).show();
+            }
+        }.start();
     }
 }
