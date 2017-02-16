7fa33951f08ed44c7a31220334505b9a025336db
Federico Menozzi <federicogmenozzi@gmail.com>
2017-02-16 12:17:59 -0500

    Draw axis tick values

--- hw2/app/src/main/java/com/example/menozzi/hw2/views/PlotView.java
+++ hw2/app/src/main/java/com/example/menozzi/hw2/views/PlotView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -31,6 +32,7 @@ public class PlotView extends View {
     Axis mYAxis;
 
     Paint mGridPaint = new Paint();
+    Paint mTickPaint = new Paint();
 
     public PlotView(Context context) {
         super(context);
@@ -69,7 +71,7 @@ public class PlotView extends View {
 
         int margin = strokeWidth/2;
 
-        LTRB bounds = new LTRB(margin+100, margin+200, w-margin-100, h-margin-300);
+        LTRB bounds = new LTRB(margin+150, margin+50, w-margin-50, h-margin-100);
 
         // Draw outer grid borders
         canvas.drawRect(bounds.l, bounds.t, bounds.r, bounds.b, mGridPaint);
@@ -87,5 +89,37 @@ public class PlotView extends View {
             int y = i*yInterval + bounds.t;
             canvas.drawLine(bounds.l, y, bounds.r, y, mGridPaint);
         }
+
+        mTickPaint.setColor(Color.GRAY);
+        mTickPaint.setAntiAlias(true);
+        mTickPaint.setTextSize(32);
+        mTickPaint.setTextAlign(Paint.Align.CENTER);
+
+        int leftAfterPadding = bounds.l - 50;
+        int bottomAfterPadding = bounds.b + 60;
+
+        // Draw x-axis tick values
+        canvas.drawText(String.valueOf(mXAxis.min), bounds.l, bottomAfterPadding, mTickPaint);
+        for (int i = 1; i <= mXAxis.getNumTicks(); i++) {
+            int x = i*xInterval + bounds.l;
+            String tickValue = String.valueOf(mXAxis.min + i*mXAxis.resolution);
+            canvas.drawText(tickValue, x, bottomAfterPadding, mTickPaint);
+        }
+        canvas.drawText(String.valueOf(mXAxis.max), bounds.r, bottomAfterPadding, mTickPaint);
+
+        mTickPaint.setTextAlign(Paint.Align.RIGHT);
+
+        Rect textBounds = new Rect();
+        mTickPaint.getTextBounds("0", 0, 1, textBounds);
+        int halfTextSize = textBounds.height()/2;
+
+        // Draw y-axis tick values
+        canvas.drawText(String.valueOf(mYAxis.min), leftAfterPadding, bounds.b + halfTextSize, mTickPaint);
+        for (int i = 1; i <= mYAxis.getNumTicks(); i++) {
+            int y = i*yInterval + bounds.t + halfTextSize;
+            String tickValue = String.valueOf(mYAxis.min + (mYAxis.max-i)*mYAxis.resolution);
+            canvas.drawText(tickValue, leftAfterPadding, y, mTickPaint);
+        }
+        canvas.drawText(String.valueOf(mYAxis.max), leftAfterPadding, bounds.t + halfTextSize, mTickPaint);
     }
 }
