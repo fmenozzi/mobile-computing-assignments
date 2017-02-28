a0c2400fc1bcc5264b6749fb238bcd27f6aee372
Federico Menozzi <federicogmenozzi@gmail.com>
2017-02-27 20:28:32 -0500

    Block on unavaiable sensors

--- hw2/app/src/main/java/com/example/menozzi/hw2/activities/MainActivity.java
+++ hw2/app/src/main/java/com/example/menozzi/hw2/activities/MainActivity.java
@@ -10,28 +10,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.menozzi.hw2.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    SensorManager mSensorManager;
+    Sensor mLightSensor;
+    Sensor mAccelSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor lightSensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
-        Sensor accelSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mAccelSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        if (lightSensor != null) {
+        if (mLightSensor != null) {
             TextView lightStatusView = (TextView) findViewById(R.id.light_status_textview);
             lightStatusView.setText("Present");
             lightStatusView.setTextColor(ContextCompat.getColor(this, R.color.colorPresent));
 
-            float rng = lightSensor.getMaximumRange();
-            float res = lightSensor.getResolution();
-            float dly = lightSensor.getMinDelay();
+            float rng = mLightSensor.getMaximumRange();
+            float res = mLightSensor.getResolution();
+            float dly = mLightSensor.getMinDelay();
 
             ((TextView) findViewById(R.id.light_range_textview)).setText(String.valueOf(rng));
             ((TextView) findViewById(R.id.light_resolution_textview)).setText(String.valueOf(res));
@@ -40,16 +45,20 @@ public class MainActivity extends AppCompatActivity {
             TextView lightStatusView = (TextView) findViewById(R.id.light_status_textview);
             lightStatusView.setText("Not Present");
             lightStatusView.setTextColor(ContextCompat.getColor(this, R.color.colorAbsent));
+
+            ((TextView) findViewById(R.id.light_range_textview)).setText("N/A");
+            ((TextView) findViewById(R.id.light_resolution_textview)).setText("N/A");
+            ((TextView) findViewById(R.id.light_delay_textview)).setText("N/A");
         }
 
-        if (accelSensor != null) {
+        if (mAccelSensor != null) {
             TextView accelSensorView = (TextView) findViewById(R.id.accel_status_textview);
             accelSensorView.setText("Present");
             accelSensorView.setTextColor(ContextCompat.getColor(this, R.color.colorPresent));
 
-            float rng = accelSensor.getMaximumRange();
-            float res = accelSensor.getResolution();
-            float dly = accelSensor.getMinDelay();
+            float rng = mAccelSensor.getMaximumRange();
+            float res = mAccelSensor.getResolution();
+            float dly = mAccelSensor.getMinDelay();
 
             ((TextView) findViewById(R.id.accel_range_textview)).setText(String.valueOf(rng));
             ((TextView) findViewById(R.id.accel_resolution_textview)).setText(String.valueOf(res));
@@ -58,12 +67,21 @@ public class MainActivity extends AppCompatActivity {
             TextView accelSensorView = (TextView) findViewById(R.id.accel_status_textview);
             accelSensorView.setText("Not Present");
             accelSensorView.setTextColor(ContextCompat.getColor(this, R.color.colorAbsent));
+
+            ((TextView) findViewById(R.id.accel_range_textview)).setText("N/A");
+            ((TextView) findViewById(R.id.accel_resolution_textview)).setText("N/A");
+            ((TextView) findViewById(R.id.accel_delay_textview)).setText("N/A");
         }
     }
 
     void buttonCallback(View v) {
-        Intent intent = new Intent(this, PlotActivity.class);
-        intent.setData(Uri.parse((v.getId() == R.id.light_sensor_button) ? "Light" : "Accelerometer"));
-        startActivity(intent);
+        boolean isLightButton = (v.getId() == R.id.light_sensor_button);
+        if ((isLightButton && mLightSensor == null) || (!isLightButton && mAccelSensor == null)) {
+            Toast.makeText(this, "Sensor unavailable", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, PlotActivity.class);
+            intent.setData(Uri.parse(isLightButton ? "Light" : "Accelerometer"));
+            startActivity(intent);
+        }
     }
 }
